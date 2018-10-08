b6e8b3e15e92bbfaec71ca9ce3dcd5231671a072
Batto1300 <tommasoricci96@gmail.com>
2018-10-08 13:46:19 +0100

    reduce datasets

--- datasets/reduce_datasets.py
+++ datasets/reduce_datasets.py
@@ -1,12 +1,12 @@
 import pandas as pd
-
+import os 
 
 EU_COUNTRIES_FILE = 'eu_countries.csv'
 GDPPC_FILE = 'GDPPC-2010-constant.csv'
 GDPPC_OUTPUT_FILE = 'GDPPC-2010-constant-reduced.csv'
 LATITUDE_FILE = 'latitude-countries.csv'
 LATITUDE_OUTPUT_FILE = 'latitude-countries-reduced.csv'
-CAPACITY_FILE = 'all-energy-capacity-eu-adj.csv'
+CAPACITY_FILE = 'all-energy-capacity-eu.csv'
 CAPACITY_OUTPUT_FILE = 'all-energy-capacity-reduced-eu.csv'
 JOIN_COLUMN = 'country'
 JOIN_TYPE = 'inner'
@@ -27,19 +27,26 @@ def reduce_to_eu(dataframe_to_reduce, output_file):
                       how=JOIN_TYPE, on=JOIN_COLUMN)
     # write the result on file
     gdp_eu.to_csv(path_or_buf=output_file)
+    # delete old dataset and rename the new one as the old
+
+def replace_file(file_a, file_b):
+    os.remove(file_a)
+    os.rename(file_b,file_a)
 
 # Reduce GDPPC dataset
-gdppc_dataframe = pd.read_csv(GDPPC_FILE, skiprows=2)
+gdppc_dataframe = pd.read_csv(GDPPC_FILE)
 reduce_to_eu(gdppc_dataframe, GDPPC_OUTPUT_FILE)
+replace_file(GDPPC_FILE,GDPPC_OUTPUT_FILE)
 # Reduce LATITUDE dataset
+# The LATITUDE dataset will raise a Assertion error
 latitude_dataframe = pd.read_csv(LATITUDE_FILE)
 latitude_dataframe.rename(
     columns={'country': 'country_code', 'name': 'country'}, inplace=True)
-reduce_to_eu(latitude_dataframe, LATITUDE_OUTPUT_FILE)
+#reduce_to_eu(latitude_dataframe, LATITUDE_OUTPUT_FILE)
+#replace_file(LATITUDE_FILE,LATITUDE_OUTPUT_FILE)
 # Reduce CAPACITY dataset
 capacity_dataframe = pd.read_csv(CAPACITY_FILE)
-"""column renamed manually because the following doesn't seem to work,it may be a bug:
-            capacity_dataframe.rename(columns={'Country/area':'country'})
-"""
+#column renamed manually because the following doesn't seem to work,it may be a bug:
+#        capacity_dataframe.rename(columns={'Country/area':'country'})
 reduce_to_eu(capacity_dataframe, CAPACITY_OUTPUT_FILE)
-
+replace_file(CAPACITY_FILE, CAPACITY_OUTPUT_FILE)
