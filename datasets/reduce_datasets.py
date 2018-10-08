4c0570dca444d1f2dcb66ad0db6668cd5f4724ec
Batto1300 <tommasoricci96@gmail.com>
2018-10-08 17:22:07 +0100

    fixed index bug where the index was saved to the csv after the join

--- datasets/reduce_datasets.py
+++ datasets/reduce_datasets.py
@@ -23,30 +23,36 @@ def reduce_to_eu(dataframe_to_reduce, output_file):
         set(dataframe_to_reduce.country.unique())
     assert not countries_diff, countries_diff
     # perform the join
-    gdp_eu = pd.merge(dataframe_to_reduce, nations,
+    dataframe_reduced = pd.merge(dataframe_to_reduce, nations,
                       how=JOIN_TYPE, on=JOIN_COLUMN)
     # write the result on file
-    gdp_eu.to_csv(path_or_buf=output_file)
-    # delete old dataset and rename the new one as the old
+    print(dataframe_reduced.head())
+    dataframe_reduced.to_csv(path_or_buf=output_file, index=False)
 
 def replace_file(file_a, file_b):
+    """substitute contents of file_a with file_b keeping the name of file_b
+    @param file_a: path of the file which contents are to keep
+    @param file_b: path of the file which name is to keep
+    """
     os.remove(file_a)
     os.rename(file_b,file_a)
 
-# Reduce GDPPC dataset
-gdppc_dataframe = pd.read_csv(GDPPC_FILE)
-reduce_to_eu(gdppc_dataframe, GDPPC_OUTPUT_FILE)
-replace_file(GDPPC_FILE,GDPPC_OUTPUT_FILE)
-# Reduce LATITUDE dataset
-# The LATITUDE dataset will raise a Assertion error
-latitude_dataframe = pd.read_csv(LATITUDE_FILE)
-latitude_dataframe.rename(
-    columns={'country': 'country_code', 'name': 'country'}, inplace=True)
-#reduce_to_eu(latitude_dataframe, LATITUDE_OUTPUT_FILE)
-#replace_file(LATITUDE_FILE,LATITUDE_OUTPUT_FILE)
-# Reduce CAPACITY dataset
-capacity_dataframe = pd.read_csv(CAPACITY_FILE)
-#column renamed manually because the following doesn't seem to work,it may be a bug:
-#        capacity_dataframe.rename(columns={'Country/area':'country'})
-reduce_to_eu(capacity_dataframe, CAPACITY_OUTPUT_FILE)
-replace_file(CAPACITY_FILE, CAPACITY_OUTPUT_FILE)
+if __name__ == "__main__":
+    # Reduce GDPPC dataset
+    print("============GDPPC===============")
+    gdppc_dataframe = pd.read_csv(GDPPC_FILE)
+    gdppc_dataframe.rename(columns={'Country':'country'},inplace=True)
+    reduce_to_eu(gdppc_dataframe, GDPPC_OUTPUT_FILE)
+    replace_file(GDPPC_FILE,GDPPC_OUTPUT_FILE)
+    # Reduce LATITUDE dataset
+    print("============LATITUDE===============")
+    latitude_dataframe = pd.read_csv(LATITUDE_FILE)
+    latitude_dataframe.rename(columns={'name':'country'},inplace=True)
+    reduce_to_eu(latitude_dataframe, LATITUDE_OUTPUT_FILE)
+    replace_file(LATITUDE_FILE,LATITUDE_OUTPUT_FILE)
+    # Reduce CAPACITY dataset
+    print("============CAPACITY===============")
+    capacity_dataframe = pd.read_csv(CAPACITY_FILE)
+    capacity_dataframe.rename(columns={'Country/area':'country'},inplace=True)
+    reduce_to_eu(capacity_dataframe, CAPACITY_OUTPUT_FILE)
+    replace_file(CAPACITY_FILE, CAPACITY_OUTPUT_FILE)
