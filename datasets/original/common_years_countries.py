50b7e350c0fe8051ba0c7f0397f8ea708eb819be
mir1995 <mir00021@hotmail.com>
2018-10-27 23:08:28 +0100

    COMMON YEARS AND COUNTRIES The script now outpus the common years (2000-2015) and common countries into a single csv file with headers "year" and "country". The code can be improved as it is very repetitive but now we can work on the analysis

--- datasets/original/common_years_countries.py
+++ datasets/original/common_years_countries.py
@@ -1,66 +1,139 @@
-import country_converter 
+import country_converter
 import pandas as pd
-import csv 
+import csv
 import functools
 
-# list of european countries belonging to ...
+
+
+# list of the European Union countries
 european_countries = country_converter.CountryConverter().EU28
-
-#list to append sets of countries from dataframes
+# lists to store countries and years from each data set. Item Type = set
 european_countries = [set(european_countries["name_short"])]
+years = []
 
 
-""" For ENERGY CAPACITY"""
+# For ENERGY CAPACITY
 ENERGY_CAPACITY = "all_energy_capacity.csv"
-df_energy_capacity = pd.read_csv(ENERGY_CAPACITY, usecols=["Country"])
-european_countries.append(set(df_energy_capacity["Country"]))
+
+# countries:
+df_countries = pd.read_csv(ENERGY_CAPACITY, usecols=["Country"])
+european_countries.append(set(df_countries["Country"]))
+
+# years:
+df_years = pd.read_csv(ENERGY_CAPACITY, nrows=0)
+header_years = df_years.columns.values
+# filter only for years. type = string
+header_years = list(filter(lambda i: i.isdigit(), header_years))
+# type = integer - does it matter?
+header_years = list(map(int, header_years))
+# append column_years to list as set type
+years.append(set(header_years))
 
 
-""" For WIND_PRODUCTION """
+# For WIND_PRODUCTION
 WIND_PRODUCTION = "wind_production.csv"
 
-header = []
-with open(WIND_PRODUCTION, 'r') as csv_file:
-    # set for reading mode
-    csv_reader = csv.reader(csv_file)
-    # get only first line - the header row
-    header = next(csv_reader) # this is the name of the file
-    # append row (now a list of column indexes) as a set to headers
-    header.append(header)
+# countries:
+df_countries = pd.read_csv(WIND_PRODUCTION, nrows=0)
+# convert dataframe object to list
+header_countries = list(df_countries.columns.values)
+# countries appear as ISO2 code - convert to full name
+header_countries = country_converter.convert(
+    names=header_countries, to='name_short', not_found=None)
+# Issue: UK and EL (Greece) were not found:
+header_countries.append('United Kingdom')
+header_countries.append('Greece')
+# append header_countries to list as set type
+european_countries.append(set(header_countries))
 
-header = country_converter.convert(names=header, to='name_short', not_found=None)
-header.append('United Kingdom')
-header=set(header)
-# appending countries
-european_countries.append(header)
+# years:
+df_years = pd.read_csv(WIND_PRODUCTION, usecols=["Year"])
+# the column contains only years already as integers
+years.append(set(df_years["Year"]))
 
 
-""" For ENERGY CONSUMPTION """
+# For ENERGY CONSUMPTION
 ENERGY_CONSUMPTION = "energy_consumption.csv"
 
-df_energy_consumption = pd.read_csv(ENERGY_CONSUMPTION)
-# inspect
-print(df_energy_consumption.head())
+# countries:
+df_countries = pd.read_csv(ENERGY_CONSUMPTION)
 # countries are in the first column - append it to list
-european_countries.append(set(df_energy_consumption.iloc[:,0]))
+european_countries.append(set(df_countries.iloc[:, 0]))
+
+# years:
+df_years = pd.read_csv(ENERGY_CONSUMPTION, nrows=3) # note if nrows is 3 pandas does no recognise the headers and returns thems as floats
+# years are in row index 1
+df_years = list(df_years.iloc[1])
+# filter only the years - some years are floats (.0)
+header_years = []
+for item in df_years:
+    try:
+        year = int(item)
+        header_years.append(year)
+    except ValueError:
+        pass
+# append to list
+years.append(set(header_years))
 
 
-""" For GDP """ 
-GDP = "GDP_2010_constant.csv" 
-df_gdp = pd.read_csv(GDP)
-# inspect 
-print(df_gdp.head())
+# For GDP
+GDP = "GDP_2010_constant.csv"
+
+# countries:
+df_countries = pd.read_csv(GDP)
 # countrie are in the second column
-european_countries.append(set(df_gdp.iloc[:,1]))
+european_countries.append(set(df_countries.iloc[:, 1]))
+
+# years:
+df_years = pd.read_csv(GDP, nrows=3)
+# years are in row index 1
+header_years = list(df_years.iloc[1])
+# filter only for years. type = string
+header_years = list(filter(lambda i: i.isdigit(), header_years))
+# type = integer -  when doing the intersection we need all years in the sampe type
+header_years = set(map(int, header_years)) #  can it be as set?
+# append column_years to list as set type
+years.append(header_years)
 
 
-""" For Politics """
-POLITICS = "CPDS_1960_2016_Update-2018.csv" 
-df_politics = pd.read_csv(POLITICS, usecols=["country"])
-# append as a set to list
-european_countries.append(set(df_politics["country"]))
-# sequential intersection on the list of sets    
-common_countries = functools.reduce(lambda A, B: A.intersection(B), european_countries) 
-# common years =
+# For Politics
+POLITICS = "CPDS_1960_2016_Update-2018.csv"
 
-print(common_countries)
+# countries & years
+df_countries = pd.read_csv(POLITICS, usecols=["year","country"])
+# append countries as a set to list
+european_countries.append(set(df_countries["country"]))
+# append years as a set to list
+df_years = list(df_countries["year"])
+header_years = []
+for item in df_years:
+    try:
+        year = int(item)
+        header_years.append(year)
+    except ValueError:
+        pass
+years.append(set(header_years))
+
+
+# sequential intersection on the list of sets for countries and years
+common_countries = functools.reduce(
+    lambda A, B: A.intersection(B), european_countries)
+
+common_years = functools.reduce(
+    lambda A, B: A.intersection(B), years)
+
+common_countries = sorted(list(common_countries))
+
+common_years = sorted(list(common_years))
+
+data = {"country": common_countries, "year": common_years}
+
+# write to a csv file
+with open("common_countries_and_years.csv", 'w') as f: 
+    # writw to file with w
+    w = csv.writer(f, delimiter = ',')
+    # header names
+    w.writerow(data.keys())
+    # countries and years
+    w.writerows(zip(*data.values()))
+        
