dac2542b2f721c9e233003fad2a85450077e3a38
mir1995 <mir00021@hotmail.com>
2018-11-02 23:05:35 +0000

    Adding comments to script There is still one script that needs comments to be addedd, the one retrieving the common years and countries to all dataframes.

--- datasets/SolarAnalysis/exploratory_analysis.py
+++ datasets/SolarAnalysis/exploratory_analysis.py
@@ -1,27 +1,38 @@
-""" This script imports all the cleaned dataframes 
-    and merges them into a single multilevel index dataframe.
+""" This script imports all the cleaned dataframes
+    on cumulative energy capacity, energy consumption
+    and meridian distance from the equator.
+    The dataframes are then merged into a single 
+    multilevel index dataframe ready for exploratory 
+    analysis.
+    Note: by storing the dataframes into a dictionary 
+    with the key being an identifies, then by 
+    applying the pandas concatenate function onto 
+    the dataframe, the keys will be the indexes name 
+    for the highest level of the multilevel indexed 
+    merged dataframe.
 """
 
 import pandas as pd
 
+# assign the names of the cleaned data sets to a variable
 ENERGY_CAPACITY = "SolarAnalysis/Cleaning/cleaned_all_energy_capacity.csv"
 ENERGY_CONSUMPTION = "SolarAnalysis/Cleaning/cleaned_energy_consumption.csv"
 MERIDIAN_DISTANCE = "SolarAnalysis/Cleaning/cleaned_meridian_countries.csv"
 
 
-# group the filenames in a list with a name indentifier
+# group the filenames (values) in a dictionary with a name indentifier (key)
 dataframes = {"solar": ENERGY_CAPACITY, "consumption": ENERGY_CONSUMPTION,
               "meridian": MERIDIAN_DISTANCE}
 
-# use a for loop to import all files
+# loop over all key,value pairs of the dataframes dictionary
 for key,value in dataframes.items():
+    # import the csv file into a dataframe & set the country column as index
     df = pd.read_csv(value, index_col=['country'])
+    # replace the filename with the respective dataframe
     dataframes[key] = df
 
-# concatenate on "dataframes" along the columns
+# concatenate the "dataframes" along the columns
+DATA = pd.concat(dataframes, axis=1, join="inner")
 # the output is a multi_level index with the key as top level
 # the inner join will join on the common row indexes country
-DATA = pd.concat(dataframes, axis=1, join="inner")
-
-
-# u might want to save it elsewhere so as not to repeat the process each time
+# we're ready for exploratory analysis
