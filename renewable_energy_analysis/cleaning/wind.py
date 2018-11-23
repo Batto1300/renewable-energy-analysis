3261147180443cdd4db39ce6ed806666e8d58e00
RichardMarkNagy <RichardMarkNagy@users.noreply.github.com>
2018-11-23 14:27:13 +0000

    Update wind.py

--- renewable_energy_analysis/cleaning/wind.py
+++ renewable_energy_analysis/cleaning/wind.py
@@ -12,25 +12,25 @@ Wind = fn.OriginalPaths.WIND
 Cleaned_Wind = fn.CleanedPaths.WIND
 Years = fn.CleanedPaths.YEARS
 Countries = fn.CleanedPaths.COUNTRIES
-#Hardcoded Strings
-Country_Column_Name = 'country'
-Years_Column_Name = 'years'
 
 #Importing Files
 df_wind = pd.read_csv(open(Wind))
-years_df = open(Years).readlines()[1:]
-countries_df = pd.read_csv(Countries)
-
-#Data Formatting
+#Removing Redundant Columns
 to_drop = ['Time','step','Date','Month','Day','Hour']
 df_wind.drop(to_drop, inplace=True, axis=1)
+#Renaming Columns
 df_wind.rename(columns={'PL':'Poland','ES':'Spain','UK':'United Kingdom','CZ':'Czech Republic','BG':'Bulgaria','NO':'Norway',
                    'RO':'Romania','DK':'Denmark','HU':'Hungary','EE':'Estonia','CH':'Switzerland', 'IE':'Ireland',
                    'SE':'Sweden','PT':'Portugal','AT':'Austria','BE':'Belgium','EL':'Greece','FI':'Finland','LT':'Lithuania',
                    'LU':'Luxembourg','LV':'Latvia','SK':'Slovakia','NL':'Netherlands','IT':'Italy','DE':'Germany',
                    'FR':'France','SI':'Slovenia','HR':'Croatia'},inplace=True)
-
-df_wind = df_wind.isin(countries_df)
+#Filter by Common Years
+years_df = open(Years).readlines()[1:]
 df_wind = df_wind[df_wind['Year'].isin(years_df)]
-
-print(df_wind.head(5))
+#Calculating Mean Per Year Per Country
+def MeanWind(x):
+    return df_wind.groupby('Year').mean()
+x = df_wind.columns
+print(MeanWind(x))
+#Filter By Common Countries
+countries_list = pd.read_csv(Countries, engine='python')
