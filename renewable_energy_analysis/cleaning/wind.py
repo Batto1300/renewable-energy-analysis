3b6f22609d795d3da2477dba74cd2c3e6f612ed2
RichardMarkNagy <RichardMarkNagy@users.noreply.github.com>
2018-11-23 12:43:36 +0000

    Update wind.py

--- renewable_energy_analysis/cleaning/wind.py
+++ renewable_energy_analysis/cleaning/wind.py
@@ -2,41 +2,35 @@ import file_names as fn
 import country_converter as cc
 import numpy as np
 import pandas as pd
+import csv
 
 #Options for Pandas
 pd.set_option('display.width', 200000)
 pd.set_option('max.columns', 200)
-
 #File Paths
 Wind = fn.OriginalPaths.WIND
 Cleaned_Wind = fn.CleanedPaths.WIND
 Years = fn.CleanedPaths.YEARS
 Countries = fn.CleanedPaths.COUNTRIES
+#Hardcoded Strings
+Country_Column_Name = 'country'
+Years_Column_Name = 'years'
 
-
-#Wind Production List
+#Importing Files
 df_wind = pd.read_csv(open(Wind))
-#Importing Common Years List
-years_list = open(Years).readlines()[1:]
-#Common years in wind data
-years_list = list(map(lambda x: x[:-1],years_list))
-df_wind = df_wind[df_wind['Year'].isin(years_list)]
-#Indexing Based on Common Years
-df_wind.set_index('Year', drop=True, inplace=True)
-#Removing Redundant Data
+years_df = open(Years).readlines()[1:]
+countries_df = pd.read_csv(Countries)
+
+#Data Formatting
 to_drop = ['Time','step','Date','Month','Day','Hour']
 df_wind.drop(to_drop, inplace=True, axis=1)
-#Reshaping Data
 df_wind.rename(columns={'PL':'Poland','ES':'Spain','UK':'United Kingdom','CZ':'Czech Republic','BG':'Bulgaria','NO':'Norway',
                    'RO':'Romania','DK':'Denmark','HU':'Hungary','EE':'Estonia','CH':'Switzerland', 'IE':'Ireland',
                    'SE':'Sweden','PT':'Portugal','AT':'Austria','BE':'Belgium','EL':'Greece','FI':'Finland','LT':'Lithuania',
                    'LU':'Luxembourg','LV':'Latvia','SK':'Slovakia','NL':'Netherlands','IT':'Italy','DE':'Germany',
                    'FR':'France','SI':'Slovenia','HR':'Croatia'},inplace=True)
-df_wind = pd.DataFrame.transpose(df_wind)
 
+df_wind = df_wind.isin(countries_df)
+df_wind = df_wind[df_wind['Year'].isin(years_df)]
 
-#Countries
-df_countries = pd.read_csv(open(Countries))
-
-
-print(df_wind)
+print(df_wind.head(5))
