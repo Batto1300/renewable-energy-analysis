04e87f144fccce94053bec0c053911c7bcd5be16
RichardMarkNagy <RichardMarkNagy@users.noreply.github.com>
2018-11-25 17:17:00 +0000

    style changes

--- renewable_energy_analysis/cleaning/wind.py
+++ renewable_energy_analysis/cleaning/wind.py
@@ -1,6 +1,4 @@
 import file_names as fn
-import country_converter as cc
-import numpy as np
 import pandas as pd
 import csv
 
@@ -8,13 +6,13 @@ import csv
 pd.set_option('display.width', 200000)
 pd.set_option('max.columns', 200)
 #File Paths
-Wind = fn.OriginalPaths.WIND
-Cleaned_Wind = fn.CleanedPaths.WIND
-Years = fn.CleanedPaths.YEARS
-Countries = fn.CleanedPaths.COUNTRIES
+WIND = fn.OriginalPaths.WIND
+CLEANED_WIND = fn.CleanedPaths.WIND
+YEARS = fn.CleanedPaths.YEARS
+COUNTRIES = fn.CleanedPaths.COUNTRIES
 
 #Importing Files
-df_wind = pd.read_csv(open(Wind))
+df_wind = pd.read_csv(open(WIND))
 #Removing Redundant Columns
 to_drop = ['Time','step','Date','Month','Day','Hour']
 df_wind.drop(to_drop, inplace=True, axis=1)
@@ -25,14 +23,20 @@ df_wind.rename(columns={'PL':'Poland','ES':'Spain','UK':'United Kingdom','CZ':'C
                    'LU':'Luxembourg','LV':'Latvia','SK':'Slovakia','NL':'Netherlands','IT':'Italy','DE':'Germany',
                    'FR':'France','SI':'Slovenia','HR':'Croatia'},inplace=True)
 #Filter by Common Years
-years_df = open(Years).readlines()[1:]
+years_df = open(YEARS).readlines()[1:]
 df_wind = df_wind[df_wind['Year'].isin(years_df)]
 #Calculating Mean Per Year For Each Country
 def MeanWind(x):
-    return df_wind.groupby('Year').mean()
+    return df_wind.groupby('Year').sum()
 x = df_wind.columns
 MeanWind(x)
 df_wind = MeanWind(x)
 #Transpose DataFrame to appropriate format
 df_wind_t = pd.DataFrame.transpose(df_wind)
+#Filter DataFrame by Common Countries
+df_countries = pd.read_csv(open(COUNTRIES), index_col=0)
+df_wind_t = pd.DataFrame.join(df_wind_t, df_countries, how='inner')
+#Saving cleaned data as new csv file
+df_wind_t.to_csv(CLEANED_WIND, index_label='country')
 print(df_wind_t)
+
