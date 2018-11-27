ea96e4f872f194785b5a4a8e302be2ddb089cb9e
PopoGee <sheripopo2010@yahoo.com>
2018-11-27 21:01:29 +0000

    Clean Political parties

---
+++ renewable_energy_analysis/cleaning/political_parties.py
@@ -0,0 +1,27 @@
+import pandas as pd
+import file_names as fn 
+import csv
+
+# original data (csv file) - Political parties in power for EU countries
+POLITCS_PATH= fn.OriginalPaths.POLITICS
+# countries common to all datasets
+COUNTRIES = fn.CleanedPaths.COUNTRIES
+# years common to all datasets
+YEARS = fn.CleanedPaths.YEARS
+# name of new csv file to store the new cleaned and filtered dataset
+C_POLITICS_PATH=fn.CleanedPaths.POLITICS
+
+
+# filling first column as the years 
+politics_df = pd.read_csv(open(POLITCS_PATH))
+politics_df = politics_df[['year', 'country','gov_right1', 'gov_cent1', 'gov_left1' ]]
+# mergeing counties
+countries_df = pd.read_csv(open(COUNTRIES))
+politics_df = pd.merge(politics_df,countries_df,on='country')
+# print(politics_df)
+# merging Years 
+years_df = pd.read_csv(open(YEARS))
+years_df.rename(columns={'years':'year'}, inplace=True)
+politics_df = pd.merge(politics_df,years_df,on='year')
+# print(politics_df)
+politics_df.to_csv(C_POLITICS_PATH)
