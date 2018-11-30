eb5e4c1552feb0c876c588f811d6817b434e483b
RichardMarkNagy <RichardMarkNagy@users.noreply.github.com>
2018-11-30 18:21:13 +0000

    Wind Analysis

--- renewable_energy_analysis/analysis/Wind_Capacity_vs_Wind_Production.py
+++ renewable_energy_analysis/analysis/Wind_Capacity_vs_Wind_Production.py
@@ -17,30 +17,19 @@ energy_capacity = pd.read_csv(open(ENERGY_CAPACITY), index_col=0)
 wind_capacity = energy_capacity[energy_capacity['Technology'] == 'Wind']
 #Import Wind Production DF
 wind_prod = pd.read_csv(open(WIND_PRODUCTION), index_col=0)
+wind_prod["Mean Wind Production"] = wind_prod.mean(axis=1)
+
 #Import Energy Consumption
 energy_consumption = pd.read_csv(open(ENERGY_CONSUMPTION), index_col=0, decimal=',')
 #Total wind production for each country from 2000-2015
 energy_consumption["sum"] = energy_consumption.sum(axis=1)
 #Join the two DataFrames together
-joined_wind = wind_capacity.join(energy_consumption, lsuffix='WC', rsuffix='WP')
-
-#Joining wind production with joined wind capacity and energy consumption
-joined_energy = wind_prod.join(joined_wind)
-#Creating Bins
-bin_range = (joined_energy['sum'].max() - joined_energy['sum'].min())/3
-next_bin = joined_energy['sum'].min()
-bins = []
-bins.append(next_bin)
-for _ in range(1,4):
-        next_bin += bin_range
-        bins.append(next_bin)
-bins[0]+=-1
-joined_energy['binned'] = pd.cut(joined_energy['sum'], bins, labels=['low','medium','high'])
-#Creating scatter plot
-joined_energy[joined_energy['binned'] == 'low'].plot(x='2015WP',y='2015WC', kind='scatter')
+joined_wind = wind_capacity.join(wind_prod["Mean Wind Production"], lsuffix='WC', rsuffix='WP')
+#Plotting wind production/wind capacity
+joined_wind.plot(x='Mean Wind Production', y='2015', kind='scatter')
+plt.title('Correlation between wind capacity and wind production', pad=30)
+plt.xlabel('Mean Wind Production')
+plt.ylabel('Wind Capacity')
+plt.grid()
 plt.show()
-print(joined_energy)
-
-
-
 
