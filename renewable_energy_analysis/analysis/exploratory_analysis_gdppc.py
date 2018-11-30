7210cf6e8d66e632b828a9b3bf066b283610f6b2
Batto1300 <tommasoricci96@gmail.com>
2018-11-30 19:49:45 +0000

    better style and logic

--- renewable_energy_analysis/analysis/exploratory_analysis_gdppc.py
+++ renewable_energy_analysis/analysis/exploratory_analysis_gdppc.py
@@ -3,27 +3,37 @@ import matplotlib.pyplot as plt
 import numpy.polynomial as poly
 import file_names as fn
 
+ENTER_MSG = "Enter 1 for GDP and 2 for GDPPC"
 GDP_FILE = fn.CleanedPaths.GDP
+GDPPC_FILE = fn.CleanedPaths.GDPPC
 ENERGY_CAPACITY_FILE = fn.CleanedPaths.CAPACITY
 ENERGY_CONSUPTION_FILE = fn.CleanedPaths.CONSUMPTION
 
-gdp = pd.read_csv(GDP_FILE, index_col = 0)
-capacity = pd.read_csv(ENERGY_CAPACITY_FILE, index_col=0)
-consumption = pd.read_csv(ENERGY_CONSUPTION_FILE,decimal=',',index_col=0)
-capacity_reduced = capacity.drop('Technology',axis=1)
-capacity_reduced = capacity.groupby('country').sum()
-capacity_reduced = capacity_reduced.divide(consumption)
-common_columns = [str(x) for x in range(2000, 2015)]
-quantiles = []
-gdp_capacity = gdp.join(capacity_reduced,lsuffix='x',rsuffix='y')
-for i in range(2000, 2016):
-    gdp_column_name = "{}x".format(str(i))
-    capacity_column_name = "{}y".format(str(i))
-    gdp_column = gdp_capacity[gdp_column_name]
-    capacity_column = gdp_capacity[capacity_column_name]
-    if gdp_column.any() and capacity_column.any():
-        p = poly.Polynomial.fit(gdp_column, capacity_column,2)
-        plt.figure(i)
-        plt.plot(gdp_column, capacity_column,'o')
-        plt.plot(*p.linspace())
-plt.show()
+def plot_all_years(dataframe, normalize=True):
+        capacity = pd.read_csv(ENERGY_CAPACITY_FILE, index_col=0)
+        capacity_reduced = capacity.drop('Technology',axis=1)
+        capacity_reduced = capacity.groupby('country').sum()
+        if normalize:
+                consumption = pd.read_csv(ENERGY_CONSUPTION_FILE,decimal=',',index_col=0)
+                capacity_reduced = capacity_reduced.divide(consumption)
+        df_capacity = dataframe.join(capacity_reduced,lsuffix='x',rsuffix='y')
+        for i in range(2000, 2016):
+                metric_column_name = "{}x".format(str(i))
+                capacity_column_name = "{}y".format(str(i))
+                metric_column = df_capacity[metric_column_name]
+                capacity_column = df_capacity[capacity_column_name]
+                if metric_column.any() and capacity_column.any():
+                        p = poly.Polynomial.fit(metric_column, capacity_column,2)
+                        plt.figure(i)
+                        plt.plot(metric_column, capacity_column,'o')
+                        plt.plot(*p.linspace())
+        plt.show()
+
+if __name__ == '__main__':
+        input_ = input(ENTER_MSG)
+        if input_ == '1':
+                gdp = pd.read_csv(GDP_FILE, index_col = 0)
+                plot_all_years(gdp)
+        if input_ == '2':
+                gdppc = pd.read_csv(GDPPC_FILE, index_col = 0)
+                plot_all_years(gdppc, normalize=False)
