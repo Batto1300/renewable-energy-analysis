9318268f68cd6641deda5b4d763ba2ee58c3326f
Batto1300 <tommasoricci96@gmail.com>
2018-11-30 11:58:36 +0000

    production normalized by consumption

--- renewable_energy_analysis/analysis/exploratory_analysis_gdppc.py
+++ renewable_energy_analysis/analysis/exploratory_analysis_gdppc.py
@@ -1,20 +1,21 @@
-# %%
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy.polynomial as poly
 import file_names as fn
 
 GDP_FILE = fn.CleanedPaths.GDP
-ENERGY_CAPACITY = fn.CleanedPaths.CAPACITY
+ENERGY_CAPACITY_FILE = fn.CleanedPaths.CAPACITY
+ENERGY_CONSUPTION_FILE = fn.CleanedPaths.CONSUMPTION
 
-gdp_reduced = pd.read_csv(open(GDP_FILE), index_col = 0)
-# %%
-capacity = pd.read_csv(open(ENERGY_CAPACITY), index_col=0)
+gdp = pd.read_csv(GDP_FILE, index_col = 0)
+capacity = pd.read_csv(ENERGY_CAPACITY_FILE, index_col=0)
+consumption = pd.read_csv(ENERGY_CONSUPTION_FILE,decimal=',',index_col=0)
 capacity_reduced = capacity.drop('Technology',axis=1)
 capacity_reduced = capacity.groupby('country').sum()
+capacity_reduced = capacity_reduced.divide(consumption)
 common_columns = [str(x) for x in range(2000, 2015)]
 quantiles = []
-gdp_capacity = gdp_reduced.join(capacity_reduced,lsuffix='x',rsuffix='y')
+gdp_capacity = gdp.join(capacity_reduced,lsuffix='x',rsuffix='y')
 for i in range(2000, 2016):
     gdp_column_name = "{}x".format(str(i))
     capacity_column_name = "{}y".format(str(i))
